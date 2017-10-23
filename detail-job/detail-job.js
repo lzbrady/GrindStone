5aac1582fc911a5ab69814f3b6d0aad1a28477dc
lzbrady <lzbrady496@gmail.com>
2017-10-23 17:47:13 -0400

    Finished implementing detail-job framework, html, css, and limited js

--- detail-job/detail-job.js
+++ detail-job/detail-job.js
@@ -1,7 +1,25 @@
 $(document).ready(function () {
+    //Display first job info div on start
+    $($('.job-info-content')[0]).show();
+
+    //Toggling job info divs
     $('.job-info-btn').click(function () {
-        console.log("Test");
+        //Hide the rest of them
+        let contents = $('.job-info-content');
+        for (let i = 0; i < contents.length; i++) {
+            $(contents[i]).hide();
+        }
+        $(this).next().show();
     });
 
-    document.getElementsByClassName('.job-info-btn');
+    function getComments() {
+        let comments = $('.comments');
+        let handle = $('.comment-handle');
+        let oneComment = $('.comment');
+
+        handle.text('username');
+        oneComment.text('This is the text of a comment. Phasellus convallis sit amet nisi ac cursus. Vestibulum sit amet ligula lacus. Integer faucibus augue ut tempor cursus. Proin tortor tortor, cursus quis porta ac, mollis non justo.')
+    }
+
+    getComments();
 });
