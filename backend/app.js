da32fab3f0e152048b136e080bf20b9f688890f6
alect116 <thompsar@rose-hulman.edu>
2017-11-08 22:10:55 -0500

    added port process.env.PORT

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ router.use(methodOverride( (req) => {
     }
 }));
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const dbURI = 'mongodb://localhost/grindstonedb';
 
 require("./models/projects");
